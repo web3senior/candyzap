8b6a2bba928a2f5ebf8be0f5f7279aaf54d0e77d
Amir Rahimi <atenyun@gmail.com>
2024-04-17 21:20:27 +0330

    Update rules

--- src/routes/rules.jsx
+++ src/routes/rules.jsx
@@ -11,14 +11,17 @@ export default function About({ title }) {
           <div className="card__header">{title}</div>
           <div className="card__body">
             <ul className="d-flex flex-column" style={{ rowGap: `1rem`, listStyleType: 'space-counter' }}>
-              <li>With CandyZap, 5% of the price goes to a random holder.</li>
+              <li>CandyZap distributes 5% of the mint price to a randomly selected holder.</li>
               <li>
-                Everything operates through a verified and audited smart contract on the <b>LUKSO</b> network
+                All operations are conducted through a smart contract that has been verified and audited on the <b>LUKSO</b> network.
               </li>
-              <li>After minting, the wallet of the sender will be included in the list of ticket pools</li>
-              <li>There are no restrictions to determine a wallet address as a winner</li>
-              <li>During minting, the sender account will not be considered as a holder. However, in the next minting, it will be taken into account</li>
-              <li>The owner of the contract can update the prices for both public mint and whitelist mint</li>
+              <li>After the tokens are minted, the sender's wallet will be added to the list of ticket pools.</li>
+              <li>There are no criteria or limitations in place to designate a wallet address as a winner.</li>
+              <li>
+                It is important to note that the winner's address will remain listed in the reward pool, irrespective of whether the token is sold in a marketplace or transferred to another wallet
+                address.
+              </li>
+              <li>The owner of the contract has the ability to modify the public mint price.</li>
             </ul>
           </div>
         </div>
